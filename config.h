7b4c1de3780b09696ceacfe3b73788955bda38cd
Hayden Heroux <haydenheroux@gmail.com>
2024-07-25 23:03:05 -0400

    Change browser to Firefox

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *surfcmd[] = { "tabsurf", NULL };
+static const char *surfcmd[] = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
